febf7ae5ccf7abfae1609646e09d34850ba9e696
yashuvaishnav <yashurishu22@gmail.com>
2023-08-24 00:14:03 +0530

    navbar complete

---
+++ rct-project/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+// import { Text } from "@chakra-ui/react"
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ rct-project/src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Center, Flex } from "@chakra-ui/react"
+ 
+const links = [
+  { to: "/", title: "Home" },
+  { to: "/blog", title: "Blog" },
+  { to: "/courses", title: "Courses" },
+  { to: "/login", title: "Login" },
+  { to: "/signup", title: "Singup" }
+];
+function Navbar() {
+  return <Flex justifyContent={"space-around"} background={"gray"} color={"white"} height={"50px"} align={"center"}>
+    {
+        links.map((item)=>(
+            <Link key={item.to} to={item.to}>{item.title}</Link>
+        ))
+    }
+  </Flex>;
+}
+
+export default Navbar;
